98829e1041d94328e2480b292753f2db9956166f
curer <studentdeng@hotmail.com>
2014-03-07 18:29:45 +0800

    ibeacon

---
+++ application/controllers/api/ibeacon.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @author      studentdeng 
+ * @link        <studentdeng.github.com> 
+ * @datetime    Mar 7, 2014 
+ */
+require APPPATH . '/libraries/CUREST_Controller.php';
+
+class Ibeacon extends CUREST_Controller
+{
+    
+}
