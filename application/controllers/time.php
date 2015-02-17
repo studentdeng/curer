5402762e60ac5f32ac5302e6615f27b34df0b284
[studentdeng] <[studentdeng@hotmail.com]]>
2015-02-17 12:24:57 +0800

    Revert "Revert "fix header dup""
    This reverts commit ad3c7ce10693378a65f68aa634290ecd75827bb3.

--- application/controllers/time.php
+++ application/controllers/time.php
@@ -1,10 +1,13 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class Time extends CI_Controller
-{
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-    function __construct()
-    {
+class Time extends CI_Controller {
+
+    private $teamcola;
+
+    function __construct() {
         parent::__construct();
 
         $this->load->helper('url');
@@ -14,26 +17,51 @@ class Time extends CI_Controller
         if (!$isLogin) {
             redirect('/user/login', 'location', 301);
         }
+
+        $this->teamcola = $this->createTeamcola();
     }
 
-    function index()
-    {
-        $url = base_url("/index.php/cola/stats/category_list");
+    function createTeamcola() {
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . '/curer/index.php/cola/stats/category_list';
         $labels = file_get_contents($url);
-        $this->load->view('time/index', array('labels' => $labels));
+
+        $teamcola = array();
+        $teamcola['api'] = array(
+            'addWorklog' => '/curer/index.php/cola/worklog/add',
+            'updateWorklog' => "/curer/index.php/cola/worklog/update",
+            'getWorklog' => "/curer/index.php/cola/worklog/list",
+            'deleteWorklog' => "/curer/index.php/cola/worklog/delete",
+            'exportWorklog' => "/curer/index.php/cola/worklog/export/",
+        );
+
+        $teamcola['me'] = array(
+            'guid' => "1bf27d0f003c411193b6f8bfe28b8dfc",
+            'avatar' => "/uploads/avatar/1bf27d0f003c411193b6f8bfe28b8dfc.jpg",
+            'name' => "curer",
+            'email' => "studentdeng@hotmail.com"
+        );
+
+        $teamcola['currentTeam'] = '990de71f506043858c431e1ea41dc725';
+        $teamcola['teams'] = array(
+            'labels' => $labels,
+            'guid' => "990de71f506043858c431e1ea41dc725",
+            'name' => "Studentdeng",
+            'archive_labels' => array(),
+        );
+        
+        return $teamcola;
     }
 
-    function team()
-    {
-        $url = base_url("/index.php/cola/stats/category_list");
-        $labels = file_get_contents($url);
-        $this->load->view('time/team', array('labels' => $labels));
+    function index() {
+        $this->load->view('time/index', array('teamcola' => $this->teamcola));
     }
 
-    function stats_label()
-    {
-        $url = base_url("/index.php/cola/stats/category_list");
-        $labels = file_get_contents($url);
-        $this->load->view('time/stats_label', array('labels' => $labels));
+    function team() {
+        $this->load->view('time/team', array('teamcola' => $this->teamcola));
     }
+
+    function stats_label() {
+        $this->load->view('time/stats_label', array('teamcola' => $this->teamcola));
+    }
+
 }
